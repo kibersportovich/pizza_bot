698e344607a04a73229229b06a69ac1599c8374b
kibersportovich <kirilenckopashayandex.ru@gmail.com>
2023-09-24 23:47:15 +0300

    Delete arguments_validator.rb

--- lib/pizza_bot/validations/arguments_validator.rb
+++
@@ -1,9 +0,0 @@
-class ArgumentsValidator
-  def validates_field(_field)
-    raise ArgumentError, '' if ()
-  end
-
-  def validates_points(_field, *_args)
-    raise ArgumentError, '' if ()
-  end
-end
