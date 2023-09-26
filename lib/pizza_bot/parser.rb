2194c6729fe0f0b86e04f681133079021457de29
kibersportovich <kirilenckopashayandex.ru@gmail.com>
2023-09-26 13:49:27 +0300

    feature: add field validator

--- lib/pizza_bot/parser.rb
+++ lib/pizza_bot/parser.rb
@@ -11,6 +11,7 @@ class Parser
     str = str.split(' ', 2)
     field = str[0]
     points = str[1].scan(/[0-9]+/).map { |i| i.to_i }
+    validates_field field
     validates_points field, points
     (1..points.length - 1).step(2).map { |i| Point.new(points[i - 1], points[i]) }
   end
--- lib/pizza_bot/validations/arguments_validator.rb
+++ lib/pizza_bot/validations/arguments_validator.rb
@@ -1,8 +1,15 @@
 module ArgumentsValidator
+  def validates_field(field)
+    field = field.split('x').map { |i| i.to_i }
+    return if field[0] >= 1 && field[1] >= 1
+
+    raise ArgumentError, 'минимальный размер поля 1x1'
+  end
+
   def validates_points(field, points)
     field = field.split('x').map { |i| i.to_i }
     (0..points.size - 1).each do |i|
-      raise ArgumentError, 'вы прописали значения выходящие за размер поля' if points[i] > field[i % 2]
+      raise ArgumentError, 'размеры точек выходят за границы поля' if points[i] > field[i % 2]
     end
   end
 end
